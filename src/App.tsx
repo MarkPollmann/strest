fa53849c0519e917fa2f1fbd61681ac4d7d3359f
Oscar Franco <ospfranco@protonmail.com>
2020-02-05 08:29:37 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Sub() {
+  return <div>Im a sub component asdasdasdasdasd</div>;
+}
+
+export function App() {
+  return (
+    <div>
+      <Sub />
+    </div>
+  );
+}
