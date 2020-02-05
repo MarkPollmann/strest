f5ba8a4404feb89532cbf63dcc6a65f60f9cf1a8
Oscar Franco <ospfranco@protonmail.com>
2020-02-05 11:16:42 +0100

    hot reload configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 function Sub() {
-  return <div>Im a sub component asdasdasdasdasd</div>;
+  return <div>Im a sub component 13332389OIJASD</div>;
 }
 
-export function App() {
+export default function App() {
   return (
     <div>
       <Sub />
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -28,9 +28,17 @@
 
 import "./index.css";
 import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
+import { render } from "react-dom";
+import App from "./App";
 
-console.log("app", App);
+render(<App />, document.getElementById("app"));
 
-ReactDOM.render(<App />, document.getElementById("app"));
+declare let module: { hot: any };
+
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    const NewApp = require("./App").default;
+
+    render(<NewApp />, document.getElementById("app"));
+  });
+}
